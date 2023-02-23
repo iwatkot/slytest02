1ffc5a21e7dddda5cf9177bf7bd8cbcfce6eb3bc
iwatkot <iwatkot@gmail.com>
2023-02-23 11:54:36 +0300

    Added args to docstrings, changed the main algorithm to be more efficient (now writing frames directly to the video, without storing them in the memory.

--- create_video.py
+++ create_video.py
@@ -6,7 +6,7 @@ from random import sample
 from enum import Enum
 
 import cv2
-import numpy
+import numpy as np
 
 from log_handler import Logger, LogTemplates, runtime
 
@@ -22,28 +22,48 @@ class Constants(Enum):
     FPS = 24
 
 
-def get_frames_set(set_name: str) -> list[numpy.ndarray]:
-    """Creates a sequence of frames with base imaged overlayed by an
-    annotation."""
-    frames_set = []
+def get_blended_frame(base_frame: str, set_name: str) -> np.ndarray:
+    """Creates a blended frame from base and overlay layers from the dataset.
 
-    # Getting list of base frame files in the set directory.
-    base_frames = os.listdir(os.path.join(Constants.JPEG_DIR.value, set_name))
-    for base_frame in base_frames:
-        # Changing the filename extension to get the overlay filename.
-        overlay_frame = f"{pathlib.Path(base_frame).stem}.png"
+    Args:
+        base_frame (str): name of the base layer file in the set
+        set_name (str): name of the set of the images in the dataset
 
-        base_layer = cv2.imread(os.path.join(Constants.JPEG_DIR.value,
-                                             set_name, base_frame))
-        overlay_layer = cv2.imread(os.path.join(Constants.ANNT_DIR.value,
-                                                set_name, overlay_frame))
+    Returns:
+        np.ndarray: numpy array, which stores blended image frame.
+    """
 
-        # Blending two image objects and append it into the list.
-        frames_set.append(
-            cv2.addWeighted(base_layer, 1 - Constants.OPACITY.value,
-                            overlay_layer, Constants.OPACITY.value, 0))
+    # Changing the filename extension to get the overlay filename.
+    overlay_frame = f"{pathlib.Path(base_frame).stem}.png"
 
-    return frames_set
+    # Loading base and overlay image objects.
+    base_layer = cv2.imread(os.path.join(Constants.JPEG_DIR.value,
+                                         set_name, base_frame))
+    overlay_layer = cv2.imread(os.path.join(Constants.ANNT_DIR.value,
+                                            set_name, overlay_frame))
+
+    # Blending two image objects and append it into the list.
+    return cv2.addWeighted(base_layer, 1 - Constants.OPACITY.value,
+                           overlay_layer, Constants.OPACITY.value, 0)
+
+
+def get_frame_size(image_set: str) -> tuple[int]:
+    """Reads the metadata from the first image in the specified set of the
+    images of the dataset. Returns dimensions of the image.
+
+    Args:
+        image_set (str): name of the set of the images in the dataset
+
+    Returns:
+        tuple[int]: dimensions of the image (width, height)
+    """
+    first_frame = os.listdir(os.path.join(Constants.JPEG_DIR.value,
+                                          image_set))[0]
+    frame_width, frame_height = cv2.imread(
+        os.path.join(Constants.JPEG_DIR.value, image_set, first_frame),
+        cv2.IMREAD_ANYDEPTH).shape[1::-1]
+
+    return frame_width, frame_height
 
 
 @runtime
@@ -52,24 +72,29 @@ def create_video(sets_number: int, output_path: str) -> None:
     dataset."""
     logger.info(LogTemplates.STARTED.format(sets_number))
     try:
+        # Reading the list of the sets in the dataset and choosing specified
+        # amount of random set names.
         image_sets = sample(os.listdir(Constants.JPEG_DIR.value), sets_number)
     except FileNotFoundError:
         logger.error(LogTemplates.NO_DATASET)
         return
 
-    frames = []
-    for image_set in image_sets:
-        frames.extend(get_frames_set(image_set))
-    logger.info(LogTemplates.FRAMES_CREATED.format(len(frames)))
-
-    # Getting frame size from the first frame in the list.
-    video_height, video_width, _ = frames[0].shape
+    # Obtaining the dimensions of the result video.
+    video_width, video_height = get_frame_size(image_sets[0])
+    # Creating video objects for adding frames into.
     video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'),
                             Constants.FPS.value, (video_width, video_height))
 
-    # Writing all frames into the video object.
-    for frame in frames:
-        video.write(frame)
+    for set_name in image_sets:
+        # Iterating over each set of the image sets, reading list of the
+        # files in the set.
+        base_frames = os.listdir(os.path.join(Constants.JPEG_DIR.value,
+                                              set_name))
+        for base_frame in base_frames:
+            # Creating blended frames for each frame, writing them into video.
+            video.write(get_blended_frame(base_frame, set_name))
+
+    # Saving resulting video and write to the log.
     video.release()
     logger.info(LogTemplates.VIDEO_CREATED)
 
